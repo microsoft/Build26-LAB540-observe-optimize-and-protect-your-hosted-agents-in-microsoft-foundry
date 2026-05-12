b852340dd3df1740fe84563483b1294144de6f8a
Nitya Narasimhan <nitya@users.noreply.github.com>
2026-05-12 06:51:26 +0000

    reset 2 / added data + initial agent

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -10,6 +10,9 @@ npm install -g @github/copilot
 # echo "Installing Marp CLI ..."
 # npm install -g @marp-team/marp-cli
 
+echo "Installing uv ..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 echo "Installing Python dependencies ..."
 pip install --upgrade pip
 pip install -r requirements.txt --quiet
